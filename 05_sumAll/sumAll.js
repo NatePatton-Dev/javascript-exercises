4f7ae5d8a3af447f1b60db86ce96c34ca22dd518
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-07 09:26:38 -0500

    Complete javascript-exercises 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (typeof(start) != 'number' || typeof(end) != 'number') {
+        return 'ERROR';
+    } else if (start < end) {
+        for (let i=start; i<=end; i++) {
+            sum += i;
+        };
+        //loop start to end
+    } else {
+        for (let i=end; i<=start; i++) {
+            sum += i;
+        };
+        //loop end to start
+    };
+    return sum;
 };
 
 // Do not edit below this line

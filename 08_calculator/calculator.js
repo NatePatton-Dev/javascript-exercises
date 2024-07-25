b26a3dc0c08de4bb017c207fab859ae0c5803472
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-25 12:05:14 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(acc, curr) {
+    return acc + curr;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(function(acc, curr) {
+    return acc * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let answer = a
+  for (let i = b-1; i > 0; i--) {
+    answer *= a;
+  };
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer = 1
+  for (let i = 1; i <= a; i++) {
+    answer *= i;
+  };
+  return answer;
 };
 
 // Do not edit below this line

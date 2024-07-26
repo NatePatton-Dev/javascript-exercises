736bc1c1f02ba00280dec27b74565d3f4b6becd5
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-26 09:55:07 -0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    
+    let sorted = arr.map(function(item) {
+        if (!('yearOfDeath' in item)) {
+            item.yearOfDeath = new Date().getFullYear();
+        };
+        return item;
+    }).sort(function(a, b) {
+        return (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth);
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line

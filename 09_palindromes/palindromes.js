99b9dfcaa5e2a00cbac1297783085536def4b88b
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-26 08:55:20 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanum = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let arr = str.toLowerCase().split('').filter(function(item) {
+        return alphanum.includes(item);
+    });
+    for (let i = 0; i < ((arr.length/2)-1); i++) {
+        if (arr[i] !== arr[(arr.length-1)-i]) {
+            return false;
+        };
+    };
+    return true;
 };
 
 // Do not edit below this line

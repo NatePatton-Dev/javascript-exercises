754038320c4862bb380164ddfc96a8215aecb230
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-26 09:24:31 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    };
+    let fibArr = [0, 1];
+    for (let i = 1; i < num; i++) {
+        fibArr.push(fibArr[i]+fibArr[i-1]);
+    };
+    return fibArr[num];
 };
 
 // Do not edit below this line
